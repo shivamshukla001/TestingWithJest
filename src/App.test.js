692e14f25d7114b71d3066c7bc77f73350f079ec
ChutKaBhuka <shivamshukla6350@gmail.com>
2025-02-25 21:03:47 +0530

    Grouping Test Case

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,65 @@
-import {render, screen}from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('should first', () => { 
-  render(<App/>)
-  
+test("should first", () => {
+  render(<App />);
+
   let inputType = screen.getByRole("textbox");
 
-  let inputTypePlaceholder = screen.getByPlaceholderText("Enter Name")
+  let inputTypePlaceholder = screen.getByPlaceholderText("Enter Name");
   expect(inputType).toBeInTheDocument();
   expect(inputTypePlaceholder).toBeInTheDocument();
-  expect(inputType).toHaveAttribute('id',"id")
-  expect(inputType).toHaveAttribute('name',"username")
+  expect(inputType).toHaveAttribute("id", "id");
+  expect(inputType).toHaveAttribute("name", "username");
+});
+test("should second", () => {
+  render(<App />);
 
- })
+  let inputType = screen.getByRole("textbox");
+
+  let inputTypePlaceholder = screen.getByPlaceholderText("Enter Name");
+  expect(inputType).toBeInTheDocument();
+  expect(inputTypePlaceholder).toBeInTheDocument();
+  expect(inputType).toHaveAttribute("id", "id");
+  expect(inputType).toHaveAttribute("name", "username");
+});
+
+describe.only("UI Development Test Case", () => {
+  test("Logo Testing", () => {
+    render(<App />);
+    let input = screen.getByPlaceholderText("Enter Name");
+    expect(input).toBeInTheDocument();
+  });
+
+  test("React Testing", () => {
+    render(<App />);
+    let input = screen.getByPlaceholderText("Enter Name");
+    expect(input).toBeInTheDocument();
+  });
+
+  test("Api Testing", () => {
+    render(<App />);
+    let input = screen.getByPlaceholderText("Enter Name");
+    expect(input).toBeInTheDocument();
+  });
+});
+
+describe.skip("Ux Development Test Case", () => {
+  test("Logo Testing", () => {
+    render(<App />);
+    let input = screen.getByPlaceholderText("Enter Name");
+    expect(input).toBeInTheDocument();
+  });
+
+  test("React Testing", () => {
+    render(<App />);
+    let input = screen.getByPlaceholderText("Enter Name");
+    expect(input).toBeInTheDocument();
+  });
+
+  test("Api Testing", () => {
+    render(<App />);
+    let input = screen.getByPlaceholderText("Enter Name");
+    expect(input).toBeInTheDocument();
+  });
+});
